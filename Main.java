baf1653c054909acd33897dee7d147b474fb12ea
vasylivandriy <vasylivandriy@gmail.com>
2019-10-20 02:22:02 +0300

    first commit

---
+++ Main.java
@@ -0,0 +1,26 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+        System.out.println("Площа кола дорівнює "+Math.round(Circle.circle_square(5)*100)/100d); //заокруглюємо до 2-го знака після коми
+        System.out.println("Довжина кола дорівнює "+Math.round(Circle.circle_length(25)*100)/100d);//заокруглюємо до 2-го знака після коми
+
+        System.out.println("=======================================================================================");
+//======================================================================================================//
+        rectangle rec2 = new rectangle(5,3);
+
+        System.out.println("Площа прямокутника = "+rectangle.rec_square(rec2.a_length, rec2.b_width));
+        System.out.println("Периметр прямокутника = "+rectangle.rec_perymeter(3,3));
+
+
+        rectangle rec1 = new rectangle();
+  //      rectangle rec2 = new rectangle(2,3);
+        rectangle rec3 = new rectangle(1);
+
+
+
+
+    }
+}
