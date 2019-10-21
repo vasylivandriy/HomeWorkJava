d65c543633305aaa10504a3cbae2d397d24661f6
vasylivandriy <vasylivandriy@gmail.com>
2019-10-21 07:24:13 +0300

    first commit

--- Main.java
+++ Main.java
@@ -1,26 +1,58 @@
+
+
 public class Main {
 
     public static void main(String[] args) {
 
-
+//===========================================КОЛО===================================================
 
         System.out.println("Площа кола дорівнює "+Math.round(Circle.circle_square(5)*100)/100d); //заокруглюємо до 2-го знака після коми
         System.out.println("Довжина кола дорівнює "+Math.round(Circle.circle_length(25)*100)/100d);//заокруглюємо до 2-го знака після коми
 
         System.out.println("=======================================================================================");
-//======================================================================================================//
-    //    rectangle rec2 = new rectangle(5,3);
+//======================================ПРЯМОКУТНИК================================================================//
 
-        System.out.println("Площа прямокутника = "+rectangle.rec_square(rectangle.a_length, rectangle.b_width));
-        System.out.println("Периметр прямокутника = "+rectangle.rec_perymeter(3,3));
-
-
+  //для конструктора без параметрів
         rectangle rec1 = new rectangle();
-  //      rectangle rec2 = new rectangle(2,3);
-        rectangle rec3 = new rectangle(1);
+
+        System.out.println("конструктор без параметрів");
+        System.out.println("Довжина = "+ rec1.getA_length()+"\nwidth = "+ rec1.getB_width());
+        System.out.println("Площа прямокутника = "+ rectangle.rec_square(rec1.getA_length(),rec1.getB_width()));
+        System.out.println("Периметр прямокутника = "+rectangle.rec_perymeter(rec1.getA_length(),rec1.getB_width()));
+        System.out.println();
+        System.out.println();
 
 
+        //для конструктора з двома параметрами
+        rectangle rec2 = new rectangle(2,3);
 
+        System.out.println("конструктор з двома параметрами");
+        System.out.println("початкові значення довжини = "+ rec2.getA_length()+ " і ширини = "+rec2.getB_width());
+
+        rec2.setA_length(20);
+        rec2.setB_width(-20);
+        System.out.println("змінені значення довжини = "+ rec2.getA_length()+ " і ширини = "+rec2.getB_width());
+
+        System.out.println("Площа прямокутника = "+ rectangle.rec_square(rec2.getA_length(),rec2.getB_width()));
+        System.out.println("Периметр прямокутника = "+rectangle.rec_perymeter(rec2.getA_length(),rec2.getB_width()));
+        System.out.println();
+        System.out.println();
+
+
+//         для конструктора з одним параметром
+        System.out.println("конструктор з одним параметром");
+
+
+        rectangle rec3 = new rectangle();
+
+
+        System.out.println("довжина = "+ rec3.getA_length()+ " і ширина = "+rec3.getB_width());
+
+
+        System.out.println("Площа прямокутника = "+ rectangle.rec_square(rec3.getA_length(),rec3.getB_width()));
+        System.out.println("Периметр прямокутника = "+rectangle.rec_perymeter(rec3.getA_length(),rec3.getB_width()));
+        System.out.println();
+        System.out.println();
 
     }
 }
--- rectangle.java
+++ rectangle.java
@@ -1,37 +1,72 @@
+
 public class rectangle {
 
- static int a_length;
-static int b_width;
+    private int a_length;
+    private int b_width;
 
 
-public rectangle(){
-    this.a_length=5;
-    this.b_width=10;
+    public rectangle() {
+        this.a_length = 5;
+        this.b_width = 10;
     }
 
-public rectangle(int a_length,int b_width){
+    public rectangle(int length, int width) {
 
-    this.a_length=a_length;
-    this.b_width=b_width;
-}
-
-public rectangle(int a_length){   //сторони рівні
-    this.a_length=a_length;
-    this.b_width=a_length;
-}
-
-
- static int rec_square(int a_length, int b_width){
-
-   int rec_square = a_length*b_width;
-    return rec_square;
-}
-
-static int rec_perymeter (int a_length, int b_width){
-
-    int rec_perymeter = a_length + b_width;
-    return rec_perymeter;
-}
+        if (length > 0 && width > 0) {
+            this.a_length = length;
+            this.b_width = width;
+        } else {
+            System.out.println("Довжина та ширина прямокутника не може бути від'ємною чи дорівнювати'0', введіть інше значення:");
+        }
+    }
+   public rectangle(int a_length) {   //сторони рівні
+            if (a_length > 0) {
+                this.a_length = a_length;
+                this.b_width = a_length;
+            } else {
+                System.out.println("Довжина прямокутника не може бути від'ємною чи дорівнювати'0', введіть інше значення:");
+            }
+        }
 
 
-}
+        public int getA_length(){
+            return a_length;
+        }
+
+        public void setA_length (int a_length){
+
+            if (a_length > 0) this.a_length = a_length;
+            else System.out.println("Довжина прямокутника не може бути від'ємною чи дорівнювати'0'");
+        }
+
+        public int getB_width () {
+            return b_width;
+        }
+
+        public void setB_width ( int b_width){
+
+            if (b_width > 0) this.b_width = b_width;
+            else System.out.println("Ширина прямокутника не може бути від'ємною чи дорівнювати'0'");
+        }
+
+
+
+
+
+        static int rec_square ( int a_length, int b_width){
+
+            int rec_square = a_length * b_width;
+            return rec_square;
+        }
+
+        static int rec_perymeter ( int a_length, int b_width){
+
+            int rec_perymeter = a_length + b_width;
+            return rec_perymeter;
+        }
+
+
+
+
+
+    }
