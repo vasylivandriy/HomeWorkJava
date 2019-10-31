866d717d81e07dc54916a60960a0f849c99d67dc
vasylivandriy <vasylivandriy@gmail.com>
2019-10-31 23:13:11 +0200

    First commit

---
+++ src/Task1/Seasons.java
@@ -0,0 +1,5 @@
+package Task1;
+
+public enum Seasons {
+
+}
---
+++ src/Task2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Task2 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		
+		double a = scan.nextDouble();
+	
+		
+		
+		
+	System.out.println(a);
+	
+	}
+
+}
