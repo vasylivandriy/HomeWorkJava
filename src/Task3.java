03980da1f8778441b892e99900d41c74b0935661
vasylivandriy <vasylivandriy@gmail.com>
2019-11-02 13:45:12 +0200

    seventh commit

--- src/Task3.java
+++ src/Task3.java
@@ -1,29 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Task3 {
 
-	public static void main(String[] args) {
-		
-Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
 
-System.out.println("Input number a");
-
-int a = scan.nextInt();
-
-System.out.println("Input number b");
-
-int b = scan.nextInt();
-
-//int result = a==scan.nextInt() ? a : 123;
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
 
+		System.out.println("Input number a");
+		String a = bufferedReader.readLine();
 
-//int aa = Integer.parseInt(a);
-//int bb = Integer.parseInt(b);
+		System.out.println("Input number b");
+		String b = bufferedReader.readLine();
 
+		if (a.contains(".") || a.contains(","))
+			System.out.println("You input wrong type of number a ");
 
+		else if (b.contains(".") || b.contains(","))
+			System.out.println("You input wrong type of number b ");
 
-	//	System.out.println(result);
+		else System.out.println("Sum of these numbers = " + (Integer.parseInt(a) + Integer.parseInt(b)));
 
 
 	}
