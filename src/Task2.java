e17660298068d14c949c95af1e6ae21935be878a
vasylivandriy <vasylivandriy@gmail.com>
2019-11-02 13:33:40 +0200

    sixth commit

--- src/Task2.java
+++ src/Task2.java
@@ -1,17 +1,25 @@
+import javax.print.DocFlavor;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Task2 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
-		Scanner scan = new Scanner(System.in);
-		
-		double a = scan.nextDouble();
-	
-		
-		
-		
-	System.out.println(a);
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.println("Input number a (type int)");
+		String a = bufferedReader.readLine();
+
+		if( a.contains(".")||a.contains(",")) System.out.println("You input wrong type of number");
+		else if(Integer.parseInt(a)%2==0) System.out.println("a is a pair number");
+		else System.out.println("a is a non pair number");
+
+
+
+
 	
 	}
 
