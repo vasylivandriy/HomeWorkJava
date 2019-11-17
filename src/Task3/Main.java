e09e079e3a2ad288ae6db215aeb2de5b3890a696
vasylivandriy <vasylivandriy@gmail.com>
2019-11-18 00:57:09 +0200

    Fifth commit

--- src/Task3/Main.java
+++ src/Task3/Main.java
@@ -1,33 +1,38 @@
-//package Task3;
-//
-//import java.util.ArrayList;
-//
-//public class Main {
-//
-//    public static void main(String[] args) {
-//
-//
-//        MyArrayList myArrayList = new MyArrayList();
-//
-//        myArrayList.add("MRT");
-//
-//
-//        System.out.println(myArrayList);
-//
-//
-//
-//
-//
-//
-//
-//
-//    }
-//
-//
-//
-//}
-//
-//
-//
-//
-//
+package Task3;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        MyArrayList myArrayList = new MyArrayList();
+
+        myArrayList.add("MRT");
+        myArrayList.add("wery");
+
+        myArrayList.remove(0);
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+}
+
+
+
+
+
--- src/Task3/MyArrayList.java
+++ src/Task3/MyArrayList.java
@@ -1,37 +1,38 @@
-//package Task3;
-//
-//public class MyArrayList {
-//
-//    private final int START_SIZE = 10;
-//    private final int CUT_RATE = 4;
-//    private Object[] array = new Object[START_SIZE];
-//    private int counter = 0;
-//
-//
-//    public void add(String element) {
-//
-//
-//        public void add (T, Object){
-//            if (counter == array.length - 1)
-//                resize(array.length * 2);
-//            array[counter++] =Object;
-//        }
-//
-//
-//
-//
-//    }
-//
-//
-//    private Object resize ( int newSize){
-//
-//        Object[] newArray = new Object[newSize];
-//
-//        System.arraycopy(array, 0, newArray, 0, counter);
-//
-//        return;
-//        newArray[];
-//
-//    }
-//
-//}
+package Task3;
+
+public class MyArrayList {
+
+    private final int START_SIZE = 10;
+    private final int CUT_RATE = 4;
+    private Object[] array = new Object[START_SIZE];
+    private int counter = 0;
+
+
+    public void add(Object e) {
+        if (counter == array.length)
+            resize(array.length * 2);
+
+        array[counter++] = e;
+    }
+
+    public void remove(int index) {
+        for (int i = index; i < counter; i++)
+            array[i] = array[i + 1];
+        array[counter] = null;
+        counter--;
+        if (array.length > START_SIZE && counter < array.length / CUT_RATE)
+            resize(array.length / 2);
+
+    }
+
+    private void resize(int newLength) {
+
+        Object[] newArray = new Object[newLength];
+
+        System.arraycopy(array, 0, newArray, 0, counter - 1);
+
+        array = newArray;
+
+    }
+
+}
