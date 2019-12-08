b8d57addf08c30b5d64dfa0cc444ee215d3f58dc
vasylivandriy <vasylivandriy@gmail.com>
2019-12-08 22:55:00 +0200

    1 Commit

---
+++ src/Task2/TaskTime.java
@@ -0,0 +1,27 @@
+package Task2;
+
+public class TaskTime {
+
+    public static void main(String[] args) {
+
+
+convertDateToLocalDate();
+convertDateToLocalTime();
+convertDateToLocalDateTime();
+
+
+
+
+
+    }
+
+private static void convertDateToLocalDate(){}
+
+private static void convertDateToLocalTime(){}
+
+private static void convertDateToLocalDateTime(){}
+
+
+
+
+}
